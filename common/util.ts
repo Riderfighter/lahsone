bd908460b8861482791853ccaed32c2a504fec7b
Riderfighter <33171826+Riderfighter@users.noreply.github.com>
2019-10-13 22:00:03 -0700

    added inspirational greeting

---
+++ common/util.ts
@@ -0,0 +1,17 @@
+export default class Util {
+    private readonly possibleGreetings = [
+        "A dashboard for all things lahs",
+        "We're like bell.plus but better",
+        "More creative than the Talon and New Media Lit",
+        "Better than the school website",
+        "Superior to LACoin",
+        "Powered by react",
+        "Something something machine learning",
+        "import tensorflow",
+        "Ad astra per aspera"
+    ];
+
+    selectGreeting() {
+        return this.possibleGreetings[Math.floor(Math.random() * this.possibleGreetings.length)];
+    };
+}
