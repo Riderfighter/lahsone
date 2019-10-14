4d2f3c8b356c18f16bac6babf088d6457daf044e
_nahoy <yohan.guyomard@gmail.com>
2019-10-13 22:09:01 -0700

    the zucc ;)

--- common/util.ts
+++ common/util.ts
@@ -9,7 +9,8 @@ export default class Util {
         "Something something machine learning",
         "import tensorflow",
         "Ad astra per aspera",
-        "downloading your personal information..."
+        "downloading your personal information...",
+        "zucc is coming for your data!!"
     ];
 
     selectGreeting() {
