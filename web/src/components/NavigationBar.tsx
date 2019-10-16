0d85b3bfb00f6e6479e193fff282f845ed83d940
_nahoy <yohan.guyomard@gmail.com>
2019-10-15 19:34:45 -0700

    Nav bar actually redirects as it should

--- web/src/components/NavigationBar.tsx
+++ web/src/components/NavigationBar.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import "../styles/NavigationBar.scss"
+import { Link } from "react-router-dom";
 
 export class NavigationBar extends React.Component
 {
@@ -62,29 +63,36 @@ export class NavigationBar extends React.Component
                         {/** Selected tab gradient */}
                         <linearGradient id="NavBarGradient" x1="0%" y1="0%" x2="0%" y2="100%">
                             <stop offset="0%"   stopColor="#F3F3F300"/>
-                            <stop offset="100%" stopColor="#F3F3F3"/>
+                            <stop offset="50%" stopColor="#F3F3F3"/>
                         </linearGradient>
                         
                         {/** Unselected tab shadow */}
                         <filter id="NavBarUnselectedTab" x="0" y="-1.4vh" width="100%" height="200%">
-                            <feDropShadow stdDeviation="20" floodOpacity="0.4"/>
+                            <feDropShadow stdDeviation="30" floodOpacity="0.2"/>
                         </filter>
                     </defs>
 
                     <rect width="100%" height="25vh" y={80} filter="url(#NavBarUnselectedTab)"/>
 
                     {/** Button 1 */}
-                    <rect onClick={() => this.setSelectedTab(0)} fill="#F3F3F3" width={this.getTabSize()} height="25vh" y={80}/>
-
+                    <Link to="/Announcements">
+                        <rect onClick={() => this.setSelectedTab(0)} fill="#F3F3F3" width={this.getTabSize()} height="25vh" y={80}/>
+                    </Link>
+                    
                     {/** Button 2 */}
-                    <rect onClick={() => this.setSelectedTab(1)} fill="#F3F3F3" width={this.getTabSize()} height="25vh" y={80} x={this.getTabSize() * 1}/>
+                    <Link to="/Schedule">
+                        <rect onClick={() => this.setSelectedTab(1)} fill="#F3F3F3" width={this.getTabSize()} height="25vh" y={80} x={this.getTabSize() * 1}/>
+                    </Link>
 
                     {/** Button 3 */}
-                    <rect onClick={() => this.setSelectedTab(2)} fill="#F3F3F3" width={this.getTabSize()} height="25vh" y={80} x={this.getTabSize() * 2}/>
+                    <Link to="/Gradebook">
+                        <rect onClick={() => this.setSelectedTab(2)} fill="#F3F3F3" width={this.getTabSize()} height="25vh" y={80} x={this.getTabSize() * 2}/>
+                    </Link>
 
                     {/** Button 4 */}
-                    <rect onClick={() => this.setSelectedTab(3)} fill="#F3F3F3" width={this.getTabSize()} height="25vh" y={80} x={this.getTabSize() * 3}/>
-
+                    <Link to="/Appointments">
+                        <rect onClick={() => this.setSelectedTab(3)} fill="#F3F3F3" width={this.getTabSize()} height="25vh" y={80} x={this.getTabSize() * 3}/>
+                    </Link>
 
                     {/** Selected Tab */}
                     <path d={this.generateNavBar()} fill="url(#NavBarGradient)"/>
