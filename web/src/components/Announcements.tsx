fe41ebe6dabb5803f310f17507b153b7599afd1d
Riderfighter <33171826+Riderfighter@users.noreply.github.com>
2019-10-16 18:00:13 -0700

    Made a minor style edit on the announcement page

--- web/src/components/Announcements.tsx
+++ web/src/components/Announcements.tsx
@@ -10,7 +10,7 @@ export class Announcements extends React.Component
             <div className="app-body">
                 <div className="announcement-body">
                     <h1 className="announcement-title">
-                        This weeks's announcements
+                        THIS WEEK'S ANNOUNCEMENTS
                     </h1>
                     <p className="announcement-message">
                         Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
