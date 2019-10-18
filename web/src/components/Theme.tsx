2253f9ed6f20fa2cb3a66213ded6686bce91bf4a
_nahoy <yohan.guyomard@gmail.com>
2019-10-17 21:46:16 -0700

    Reverted back to light theme

--- web/src/components/Theme.tsx
+++ web/src/components/Theme.tsx
@@ -4,42 +4,42 @@ export default class Theme
      * Default, Light: #F3F3F3
      * Default, Dark: #2E3047
      */
-    public static Background: string = "#2E3047";
+    public static Background: string = "#F3F3F3";
 
     /**
      * Default, Light: #434343
      * Default, Dark: #82A1B0
      */
-    public static Logo: string = "#82A1B0";
+    public static Logo: string = "#434343";
 
     /**
      * Default, Light: #1F1F1F
      * Default, Dark: #B8C7DB
      */
-    public static LogoSuperscript: string = "#b8c7db";
+    public static LogoSuperscript: string = "#1F1F1F";
 
     /**
      * Default, Light: #111111
      * Default, Dark: #CAE2E3
      */
-    public static Title: string = "#cae2e3";
+    public static Title: string = "#111111";
 
     /**
      * Default, Light: #222222
      * Default, Dark: #A1B2B3
      */
-    public static Subtitle: string = "#a1b2b3";
+    public static Subtitle: string = "#222222";
 
     /**
      * Default, Light: #424242
      * Default, Dark: #A2B0B8
      */
-    public static Content: string = "#a2b0b8";
+    public static Content: string = "#424242";
 
     /**
      * Default, Light: #9CC884
      * Default, Dark: #5C7A67
      */
-    public static SchedulePie: string = "#5c7a67";
+    public static SchedulePie: string = "#9CC884";
     
 }
