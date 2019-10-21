d0cce155586661f32dc86eb98274c0d2af98449d
Riderfighter <33171826+Riderfighter@users.noreply.github.com>
2019-10-21 11:58:37 -0700

    Changed greetings

--- common/Utilities.ts
+++ common/Utilities.ts
@@ -1,16 +1,13 @@
 export default class Utilities {
     private readonly possibleGreetings = [
         "A dashboard for all things lahs",
+        "Made in two weeks!",
         "We're like bell.plus but better",
         "More creative than the Talon and New Media Lit",
         "Better than the school website",
-        "Superior to LACoin",
+        "Check your grades right in the app",
         "Powered by react",
-        "Something something machine learning",
         "import tensorflow",
-        "Ad astra per aspera",
-        "Downloading your personal information...",
-        "Zucc is coming for your data!!",
         "We did not cost thousands of dollars to produce."
     ];
 
