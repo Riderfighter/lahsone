c3a8f2eb861437632818e39033cd6cca290a1cbf
Riderfighter <33171826+Riderfighter@users.noreply.github.com>
2019-10-23 10:58:38 -0700

    Made announcements look nicer

--- web/src/components/Announcements.tsx
+++ web/src/components/Announcements.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import '../styles/Announcements.scss';
 import Theme from "./Theme";
+import dateFormat from "dateformat";
 
 
 export class Announcements extends React.Component
@@ -10,8 +11,9 @@ export class Announcements extends React.Component
         return (
             <div className="app-body">
                 <div className="announcement-body">
-                    <h1 className="announcement-title" style={{color: Theme.Title}}>
-                        THIS WEEK'S ANNOUNCEMENTS
+                    <h1 className="announcement-title" style={{color: "#ffffff"}}>
+                        {dateFormat(new Date(), "mm/dd/yy")}
+                        <b className="fa fa-angle-right"/>
                     </h1>
                     <p className="announcement-message" style={{color: Theme.Content}}>
                         Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
