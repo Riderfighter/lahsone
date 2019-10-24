109baa9082fafb4efcd0120ef2b9a21ab4533aeb
Riderfighter <33171826+Riderfighter@users.noreply.github.com>
2019-10-24 13:41:09 -0700

    Fixed announcements and allowed to toggle theme

--- web/src/components/Theme.tsx
+++ web/src/components/Theme.tsx
@@ -1,50 +1,44 @@
 export default class Theme
 {
     /**
+     *
+     */
+    public static ScheduleHighlight: string = "#EDEDED";
+    private static useLight = true;
+    /**
      * Default, Light: #F3F3F3
      * Default, Dark: #2E3047
      */
-    public static Background: string = "#F3F3F3";
-
+    public static Background: string = Theme.useLight ? "#F3F3F3" : "#2E3047";
     /**
      * Default, Light: #434343
      * Default, Dark: #82A1B0
      */
-    public static Logo: string = "#434343";
-
+    public static Logo: string = Theme.useLight ? "#434343" : "#82A1B0";
     /**
      * Default, Light: #1F1F1F
      * Default, Dark: #B8C7DB
      */
-    public static LogoSuperscript: string = "#1F1F1F";
-
+    public static LogoSuperscript: string = Theme.useLight ? "#1F1F1F" : "#B8C7DB";
     /**
      * Default, Light: #111111
      * Default, Dark: #CAE2E3
      */
-    public static Title: string = "#111111";
-
+    public static Title: string = Theme.useLight ? "#111111" : "#CAE2E3";
     /**
      * Default, Light: #222222
      * Default, Dark: #A1B2B3
      */
-    public static Subtitle: string = "#222222";
-
+    public static Subtitle: string = Theme.useLight ? "#222222" : "#A1B2B3";
     /**
      * Default, Light: #424242
      * Default, Dark: #A2B0B8
      */
-    public static Content: string = "#424242";
-
+    public static Content: string = Theme.useLight ? "#424242" : "#A2B0B8";
     /**
      * Default, Light: #9CC884
      * Default, Dark: #5C7A67
      */
-    public static SchedulePie: string = "#9CC884";
+    public static SchedulePie: string = Theme.useLight ? "#9CC884" : "#5C7A67";
 
-    /**
-     * 
-     */
-    public static ScheduleHighlight: string = "#EDEDED";
-    
 }
