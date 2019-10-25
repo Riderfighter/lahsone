a3d5df92b195a3b257d4c80fbd94b1cd942e8334
Riderfighter <33171826+Riderfighter@users.noreply.github.com>
2019-10-25 13:29:47 -0700

    Fixed gradebook a bit so that we don't have to have two classes and it's easier to do.

--- web/src/components/Gradebook.tsx
+++ web/src/components/Gradebook.tsx
@@ -3,14 +3,19 @@ import "../styles/Gradebook.scss";
 import AeriesUtilities from "../common/Aeries";
 
 export class Gradebook extends React.Component {
+    private AeriesUtil = new AeriesUtilities();
     constructor(props: any) {
         super(props);
+        this.loginTest = this.loginTest.bind(this);
+        this.handleChange = this.handleChange.bind(this);
         this.state = {
             show: false,
             showPopup: false,
             showGradebook: false,
             showtext: 'block',
-            name: ''
+            name: '',
+            email: '',
+            password: ''
         };
     }
 
@@ -35,67 +40,42 @@ export class Gradebook extends React.Component {
 
     }
 
-    render() {
-        return (
-            <div className="app-body">
-                {(this.state as any).showPopup ?
-                    <Popup/> : [<p className="app-message" id={"hide"} style={{display: (this.state as any).showtext}}>
-                        Welcome to your Gradebook {(this.state as any).name}
-                    </p>, <a className="button1" onClick={this.togglePopup.bind(this)}>Login to Aeries</a>]
-                }
-            </div>
-        );
+    handleChange(event) {
+        this.setState({value: event.target.value});
     }
-}
-
-class Popup extends React.Component {
-    state = {
-        email: '',
-        password: ''
-    };
-    private AeriesUtil = new AeriesUtilities();
-
-    constructor(props) {
-        super(props);
-        this.handleChange = this.handleChange.bind(this);
-        this.loginTest = this.loginTest.bind(this);
-        this.state = {
-            email: '',
-            password: ''
-        }
-    }
-
-    sendData = () => {
-        (this.props as any).parentCallback("Hey Popsie, How’s it going?");
-    };
 
     loginTest(event) {
         this.AeriesUtil.authenticateAeries((this.state as any).email, (this.state as any).password).then(() => {
-                console.log(this.AeriesUtil.studentGradebook.currentStudent.name);
+                console.log(this.AeriesUtil.studentGradebook.currentStudent);
             }
         );
         event.preventDefault();
     }
 
-    handleChange(event) {
-        this.setState({value: event.target.value});
-    }
-
-    render() {
+    makePopup() {
         return (
             <div className='popup'>
                 <div className='popup_inner'>
-                    {/*<h1>{(this.props as any).text}</h1>*/}
-                    {/*<button onClick={(this.props as any).closePopup}>close me</button>*/}
-                    {/*<h1>{(this.state as any).email}</h1>*/}
                     <form onSubmit={this.loginTest}>
-                        <input placeholder={"email"} value={(this.state as any).email}
+                        <input placeholder={"Email"} value={(this.state as any).email}
                                onChange={(e) => (this.setState({email: e.target.value}))}/>
                         <input placeholder={"Password"} type={'password'} value={(this.state as any).password}
                                onChange={(e) => (this.setState({password: e.target.value}))}/>
                         <input type="submit" value="Submit"/>
                     </form>
                 </div>
+            </div>
+        )
+    }
+
+    render() {
+        return (
+            <div className="app-body">
+                {(this.state as any).showPopup ?
+                    this.makePopup() : [<p className="app-message">
+                        Welcome to Gradebook
+                    </p>, <a className="button1" onClick={this.togglePopup.bind(this)}>Login to Aeries</a>]
+                }
             </div>
         );
     }
--- web/src/components/Theme.tsx
+++ web/src/components/Theme.tsx
@@ -1,7 +1,7 @@
 /**
  * CHANGE THE DIRECTORY HERE TO CHANGE THEMES
  */
-import theme from '../common/themes/default-dark.json'
+import theme from '../common/themes/default-light.json'
 
 export default class Theme
 {
