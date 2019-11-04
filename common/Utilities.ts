c0ae184e88aefa569743727ac3fb3fbd17640a16
Riderfighter <33171826+Riderfighter@users.noreply.github.com>
2019-11-04 09:27:21 -0800

    Made the greetings more meta

--- common/Utilities.ts
+++ common/Utilities.ts
@@ -1,17 +1,13 @@
 export default class Utilities {
     private readonly possibleGreetings = [
         "A dashboard for all things lahs",
-        "We're like bell.plus but better",
+        "Inspired by bell.plus",
+        "Native Aeries support",
         "More creative than the Talon and New Media Lit",
         "Better than the school website",
-        "Superior to LACoin",
         "Powered by react",
-        "Something something machine learning",
         "import tensorflow",
-        "Ad astra per aspera",
-        "Downloading your personal information...",
-        "Zucc is coming for your data!!",
-        "We did not cost thousands of dollars to produce. We cost millions."
+        "We did not cost thousands of dollars to produce."
     ];
 
     selectGreeting() {
